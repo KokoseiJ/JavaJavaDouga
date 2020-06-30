573cd9771c4eb928befb474b71f80a318ffa806b
KokoseiJ <kokoseij@gmail.com>
2020-07-01 02:16:04 +0900

    Added the test

---
+++ src/test/java/GetThumbInfoTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+public class GetThumbInfoTest {
+    @Test
+    @DisplayName("SM video test")
+    void smVideoTest() {
+        //OrangeStar - Alice in reitouko
+        assertDoesNotThrow(() -> NicoAPI.getThumbInfo("sm28242091"));
+    }
+
+    @Test
+    @DisplayName("NM video test")
+    void nmVideoTest() {
+        //
+        assertDoesNotThrow(() -> NicoAPI.getThumbInfo("nm6049209"));
+    }
+}
